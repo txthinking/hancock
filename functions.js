14e5bf3e2e98947ef28d429cfd83bdf709aa7bf3
TxThinking <cloud@txthinking.com>
2021-06-05 18:07:25 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,16 +2,20 @@
 // return ["full command", null] or [null, "error message"]
 
 function brook_server(args){
+  var s = "brook server";
   
   var listen = args.find(v=>v.name == 'listen');
   if(!listen.value) return [null, "missing listen"];
+  s += " --listen '" + listen.value + "'";
   var password = args.find(v=>v.name == 'password');
   if(!password.value) return [null, "missing password"];
+  s += " --password '" + password.value + "'";
   var tcpTimeout = args.find(v=>v.name == 'tcpTimeout');
   if(isNaN(parseInt(tcpTimeout.value))) return [null, "tcpTimeout must be int"];
+  s += " --tcpTimeout " + parseInt(tcpTimeout.value);
   var udpTimeout = args.find(v=>v.name == 'udpTimeout');
   if(isNaN(parseInt(udpTimeout.value))) return [null, "udpTimeout must be int"];
+  s += " --udpTimeout " + parseInt(udpTimeout.value);
   
-  var s = `brook server --listen ${listen.value} --password ${password.value} --tcpTimeout ${parseInt(tcpTimeout.value))} --udpTimeout ${parseInt(udpTimeout.value))}`;
   return [s, null];
 }

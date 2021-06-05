5f79a15b181b3f84e37afab19c8ec5951119c378
TxThinking <cloud@txthinking.com>
2021-06-06 04:36:48 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,32 +2,32 @@
 // return ["full command", null] or [null, "error message"]
 
 function brook_server(args){
-  var s = "brook server";
-  for(var i=0; i<args.length; i++){
+    var s = "brook server";
+    for(var i=0; i<args.length; i++){
         if(args[i].name == 'listen'){
-                if(!args[i].value){
-                        return [null, "missing listen"];
-                }
+            if(!args[i].value){
+                    return [null, "missing listen"];
+            }
             s += " --listen '" + args[i].value + "'";
         }
         if(args[i].name == 'password'){
-                if(!args[i].value){
-                        return [null, "missing password"];
-                }
+            if(!args[i].value){
+                    return [null, "missing password"];
+            }
             s += " --password '" + args[i].value + "'";
         }
         if(args[i].name == 'tcpTimeout'){
-                if(isNaN(parseInt(args[i].value))){
-                        return [null, "tcpTimeout must be int"];
-                }
+            if(isNaN(parseInt(args[i].value))){
+                    return [null, "tcpTimeout must be int"];
+            }
             s += " --tcpTimeout " + parseInt(args[i].value);
         }
         if(args[i].name == 'udpTimeout'){
-                if(isNaN(parseInt(args[i].value))){
-                        return [null, "udpTimeout must be int"];
-                }
+            if(isNaN(parseInt(args[i].value))){
+                    return [null, "udpTimeout must be int"];
+            }
             s += " --udpTimeout " + parseInt(args[i].value);
         }
-  }
-  return [s, null];
+    }
+    return [s, null];
 }

e706cac2be64eef86b8205f88ff95548aff51535
TxThinking <cloud@txthinking.com>
2021-06-06 05:03:19 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,25 +4,25 @@
 function brook_server(args){
     var s = "brook server";
     for(var i=0; i<args.length; i++){
-        if(args[i].name == 'listen'){
+        if(args[i].name == '--listen'){
             if(!args[i].value){
                     return [null, "missing listen"];
             }
             s += " --listen '" + args[i].value + "'";
         }
-        if(args[i].name == 'password'){
+        if(args[i].name == '--password'){
             if(!args[i].value){
                     return [null, "missing password"];
             }
             s += " --password '" + args[i].value + "'";
         }
-        if(args[i].name == 'tcpTimeout'){
+        if(args[i].name == '--tcpTimeout'){
             if(isNaN(parseInt(args[i].value))){
                     return [null, "tcpTimeout must be int"];
             }
             s += " --tcpTimeout " + parseInt(args[i].value);
         }
-        if(args[i].name == 'udpTimeout'){
+        if(args[i].name == '--udpTimeout'){
             if(isNaN(parseInt(args[i].value))){
                     return [null, "udpTimeout must be int"];
             }
